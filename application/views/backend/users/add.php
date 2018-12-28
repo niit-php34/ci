bae196212e380c1dc9be2c51fe4e755af1c4a70b
lrandom <luann4099@gmail.com>
2018-12-28 18:57:31 +0700

    - add form add new user

--- application/views/backend/users/add.php
+++ application/views/backend/users/add.php
@@ -2,32 +2,80 @@
   <!-- Horizontal Form -->
   <div class="box box-info">
     <div class="box-header with-border">
-      <h3 class="box-title">Horizontal Form</h3>
+      <h3 class="box-title">Add new user</h3>
     </div>
     <!-- /.box-header -->
     <!-- form start -->
     <form class="form-horizontal">
       <div class="box-body">
+
         <div class="form-group">
           <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
 
           <div class="col-sm-10">
-            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Email" name="email">
           </div>
         </div>
+
+
         <div class="form-group">
-          <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+          <label for="inputEmail3" class="col-sm-2 control-label">Fullname</label>
 
           <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Fullname" name="fullname">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="inputEmail3" class="col-sm-2 control-label">UserName</label>
+
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="UserName" name="username">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="inputEmail3" class="col-sm-2 control-label">Address</label>
+
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Address" name="address">
+          </div>
+        </div>
+
+        <div class="form-group">
+          <label for="inputEmail3" class="col-sm-2 control-label">Phone</label>
+
+          <div class="col-sm-10">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Phone" name="phone">
+          </div>
+        </div>
+
+
+        <div class="form-group">
+          <label for="inputEmail3" class="col-sm-2 control-label">Password</label>
+
+          <div class="col-sm-10">
+            <input type="password" class="form-control" id="inputEmail3" placeholder="Email" name="pwd">
+          </div>
+        </div>
+
+
+        <div class="form-group">
+          <label for="inputEmail3" class="col-sm-2 control-label">Level</label>
+
+          <div class="col-sm-10">
+            <select class="form-control" name="level">
+              <option>Staff</option>
+              <option>Admin</option>
+            </select>
           </div>
         </div>
 
       </div>
       <!-- /.box-body -->
       <div class="box-footer">
-        <button type="submit" class="btn btn-default">Reset</button>
-        <button type="submit" class="btn btn-info pull-right">Submit</button>
+        <button type="submit" name="reset" class="btn btn-default">Reset</button>
+        <button type="submit" name="submit" class="btn btn-info pull-right">Submit</button>
       </div>
       <!-- /.box-footer -->
     </form>
